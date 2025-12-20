aaf7cd713e167cfd708639a8e569fe91a5c0576a
sol-un <solovyev833@gmail.com>
2025-12-20 14:26:08 +0300

    set up snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,32 @@
+return {
+  'folke/snacks.nvim',
+  event = 'VeryLazy',
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          layout = {
+            fullscreen = false,
+            preview = 'main',
+          },
+        },
+        select = { layout = { fullscreen = false } },
+        lines = { layout = { fullscreen = false } },
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+        grep = { hidden = true },
+        grep_word = { hidden = true },
+        recent = { hidden = true, ignored = true },
+        notifications = { win = { wo = { wrap = true } } },
+      },
+      layout = {
+        fullscreen = true,
+      },
+    },
+  },
+}
