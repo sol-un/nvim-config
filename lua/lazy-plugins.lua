62d6ce6570d7806a796276671cf57ae7b1604d49
sol-un <solovyev833@gmail.com>
2025-12-20 14:18:19 +0300

    set up tokyonight

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -3,6 +3,7 @@ require('lazy').setup({ 'NMAC427/guess-indent.nvim', { import = 'plugins' } }, {
     enabled = true,
     notify = true,
   },
+  install = { colorscheme = { 'tokyonight' } },
 })
 
 -- vim: ts=2 sts=2 sw=2 et
