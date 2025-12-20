6e09df6b26f9104a1ce9116f20d96305d1100cdc
sol-un <solovyev833@gmail.com>
2025-12-20 10:47:53 +0300

    use snacks to set keymaps

--- init.lua
+++ init.lua
@@ -10,14 +10,14 @@ vim.g.have_nerd_font = true
 -- [[ Setting options ]]
 require 'options'
 
--- [[ Basic Keymaps ]]
-require 'keymaps'
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
+
+-- [[ Basic Keymaps ]]
+require 'keymaps'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
