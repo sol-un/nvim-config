39ff557f2eb72c85f4b26bb568c1a6b4742a2bb9
sol-un <solovyev833@gmail.com>
2025-12-20 15:53:55 +0300

    set up noice

--- lua/plugins/lang/treesitter.lua
+++ lua/plugins/lang/treesitter.lua
@@ -1,5 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
   lazy = false,
   build = ':TSUpdate',
+  opts = { ensure_installed = { 'bash', 'markdown', 'markdown_inline', 'regex', 'vim' } },
 }
