e7ec3cf8f275bd74743a1ebbf26bf2e37f9bbf40
sol-un <solovyev833@gmail.com>
2025-12-20 15:52:53 +0300

    place nvim-mini files into separate directory

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,10 +1,12 @@
 require('lazy').setup(
-{ 'NMAC427/guess-indent.nvim', { import = 'plugins' }, { import = 'plugins.lang' }, { import = 'plugins.git' } }, {
-  checker = {
-    enabled = true,
-    notify = true,
-  },
-  install = { colorscheme = { 'tokyonight' } },
-})
+  { 'NMAC427/guess-indent.nvim', { import = 'plugins' }, { import = 'plugins.lang' }, { import = 'plugins.git' }, { import = 'plugins.mini' } },
+  {
+    checker = {
+      enabled = true,
+      notify = true,
+    },
+    install = { colorscheme = { 'tokyonight' } },
+  }
+)
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/mini/mini-surround.lua
@@ -0,0 +1,16 @@
+local prefix = 'gz'
+
+return {
+  'nvim-mini/mini.surround',
+  opts = {
+    mappings = {
+      add = prefix .. 'a', -- Add surrounding in Normal and Visual modes
+      delete = prefix .. 'd', -- Delete surrounding
+      find = prefix .. 'f', -- Find surrounding (to the right)
+      find_left = prefix .. 'F', -- Find surrounding (to the left)
+      highlight = prefix .. 'h', -- Highlight surrounding
+      replace = prefix .. 'r', -- Replace surrounding
+      update_n_lines = prefix .. 'n', -- Update `n_lines`
+    },
+  },
+}
