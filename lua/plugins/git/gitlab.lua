d83fa5cd2f49afad39679aee2896daa0576c5037
sol-un <solovyev833@gmail.com>
2025-12-20 21:32:46 +0300

    adjust keymaps

--- lua/plugins/git/gitlab.lua
+++ lua/plugins/git/gitlab.lua
@@ -1,37 +1,71 @@
+local prefix = '<Leader>gl'
+
 return {
-  'harrisoncramer/gitlab.nvim',
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-    'nvim-lua/plenary.nvim',
-    'sindrets/diffview.nvim',
-  },
-  build = function()
-    require('gitlab.server').build(true)
-  end,
-  opts = {
-    reviewer_settings = {
-      jump_with_no_diagnostics = true,
-      diffview = {
-        imply_local = true,
-      },
+  {
+    'harrisoncramer/gitlab.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
     },
-    keymaps = {
+    build = function()
+      require('gitlab.server').build(true)
+    end,
+    opts = {
+      reviewer_settings = {
+        jump_with_no_diagnostics = true,
+        diffview = {
+          imply_local = true,
+        },
+      },
+      keymaps = {
+        global = {
+          add_assignee = prefix .. 'aa',
+          delete_assignee = prefix .. 'ad',
+          add_label = prefix .. 'la',
+          delete_label = prefix .. 'ld',
+          add_reviewer = prefix .. 'ra',
+          delete_reviewer = prefix .. 'rd',
+          approve = prefix .. 'A', -- Approve MR
+          revoke = prefix .. 'R', -- Revoke MR approval
+          merge = prefix .. 'M', -- Merge the feature branch to the target branch and close MR
+          create_mr = prefix .. 'C', -- Create a new MR for currently checked-out feature branch
+          choose_merge_request = prefix .. 'c', -- Chose MR for review (if necessary check out the feature branch)
+          start_review = prefix .. 'S', -- Start review for the currently checked-out branch
+          summary = prefix .. 's', -- Show the editable summary of the MR
+          copy_mr_url = prefix .. 'u', -- Copy the URL of the MR to the system clipboard
+          open_in_browser = prefix .. 'o', -- Openthe URL of the MR in the default Internet browser
+          create_note = prefix .. 'n', -- Create a note (comment not linked to a specific line)
+          pipeline = prefix .. 'p', -- Show the pipeline status
+          toggle_discussions = prefix .. 'd', -- Toggle the discussions window
+          toggle_draft_mode = prefix .. 'D', -- Toggle between draft mode (comments posted as drafts) and live mode (comments are posted immediately)
+          publish_all_drafts = prefix .. 'P', -- Publish all draft comments/notes
+        },
+        discussion_tree = {
+          toggle_sort_method = 'gSt',
+          toggle_date_format = 'gDt',
+          add_emoji = 'gEa',
+          delete_emoji = 'gEd',
+        },
+        popup = {
+          perform_action = '<C-s>',
+          discard_changes = '<C-k>',
+        },
+      },
       discussion_tree = {
-        toggle_sort_method = 'gSt',
-        toggle_date_format = 'gDt',
-        add_emoji = 'gEa',
-        delete_emoji = 'gEd',
-      },
-      popup = {
-        perform_action = '<C-s>',
-        discard_changes = '<C-k>',
+        position = 'left',
+        keep_current_open = true,
+        draft_mode = true,
+        tree_type = 'by_file_name',
       },
     },
-    discussion_tree = {
-      position = 'left',
-      keep_current_open = true,
-      draft_mode = true,
-      tree_type = 'by_file_name',
+  },
+  {
+    'folke/which-key.nvim',
+    opts = {
+      spec = {
+        { '<Leader>gl', group = 'Gitlab' },
+      },
     },
   },
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,19 @@
 return {
-  {
-    'folke/which-key.nvim',
-    event = 'VimEnter',
-    opts = {
-      delay = 0,
-      show_help = false,
-      preset = 'helix',
-      icons = {
-
-        mappings = vim.g.have_nerd_font,
-      },
-
-      spec = {
-        { '<leader>t', group = 'Toggle' },
-        { '<Leader>g', group = 'Git' },
-        { '<Leader>q', group = 'Quit' },
-        { '<Leader>w', group = 'Windows' },
-        { '<Leader><Tab>', group = 'Tabs' },
-      },
+  'folke/which-key.nvim',
+  event = 'VimEnter',
+  opts = {
+    delay = 0,
+    show_help = false,
+    preset = 'helix',
+    icons = {
+      mappings = vim.g.have_nerd_font,
+    },
+    spec = {
+      { '<leader>t', group = 'Toggle' },
+      { '<Leader>g', group = 'Git' },
+      { '<Leader>q', group = 'Quit' },
+      { '<Leader>w', group = 'Windows' },
+      { '<Leader><Tab>', group = 'Tabs' },
     },
   },
 }
