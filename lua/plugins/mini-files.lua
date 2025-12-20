592a4be196a68f4a40207efd03b824008b27d84c
sol-un <solovyev833@gmail.com>
2025-12-20 14:16:54 +0300

    set up mini-files

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,25 @@
+return {
+  'nvim-mini/mini.files',
+  event = 'VeryLazy',
+  opts = {
+    windows = {
+      preview = true,
+      max_number = 2,
+      width_focus = math.floor(vim.o.columns * 0.29),
+      width_preview = math.floor(vim.o.columns * 0.69),
+    },
+  },
+  keys = function()
+    local set = require('snacks').keymap.set
+    set('n', '<Leader>e', function()
+      if not require('mini.files').close() then
+        require('mini.files').open(vim.api.nvim_buf_get_name(0))
+      end
+    end, { desc = 'Explorer' })
+    set('n', '<Leader>E', function()
+      if not require('mini.files').close() then
+        require('mini.files').open(vim.uv.cwd())
+      end
+    end, { desc = 'Explorer (CWD)' })
+  end,
+}
