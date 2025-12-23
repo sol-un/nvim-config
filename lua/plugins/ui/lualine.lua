8a005fa21bc165c89a0bece4a03cb3fc6712c7ba
sol-un <solovyev833@gmail.com>
2025-12-24 00:13:08 +0300

    set up lualine, dropbar

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,5 +1,43 @@
+local function get_linters()
+  local linters = require('lint')._resolve_linter_by_ft(vim.bo.filetype)
+
+  return table.concat(linters, ', ')
+end
+
+local function get_formatters()
+  local formatters = require('conform').list_formatters(0)
+
+  local formatter_names = vim
+    .iter(formatters)
+    :map(function(formatter)
+      return formatter.name
+    end)
+    :totable()
+  return table.concat(formatter_names, ', ')
+end
+
+local function get_cwd()
+  return vim.fn.getcwd()
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = { sections = { lualine_y = { 'lsp_status' } } },
+  opts = {
+    options = {
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      globalstatus = true,
+    },
+    sections = {
+      lualine_c = { { get_cwd, icon = '' } },
+      lualine_x = {},
+      lualine_y = {
+        { 'lsp_status', icon = '', symbols = { done = '', separator = ', ', spinner = {} } },
+        { get_linters },
+        { get_formatters },
+      },
+      lualine_z = { 'location', { 'datetime', icon = '', style = '%H:%M' } },
+    },
+  },
 }
