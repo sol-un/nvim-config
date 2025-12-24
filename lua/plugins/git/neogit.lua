db6c3db3443d6726f70d3898992718722cc275f9
sol-un <solovyev833@gmail.com>
2025-12-24 09:52:38 +0300

    add neogit autocmds

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -1,3 +1,34 @@
+local fetch = vim.schedule_wrap(function()
+  require('plenary.job')
+    :new({
+      command = 'git',
+      args = { 'fetch', '--all' },
+      cwd = vim.fn.getcwd(),
+      on_exit = function(j, return_val)
+        if return_val ~= 0 then
+          vim.notify('Git fetch failed: ' .. table.concat(j:stderr_result(), '\n'), vim.log.levels.ERROR)
+        end
+      end,
+    })
+    :start()
+end)
+
+vim.api.nvim_create_autocmd('User', {
+  desc = 'Update buffers on Neogit events',
+  group = vim.api.nvim_create_augroup('neogit_buffers_update', { clear = true }),
+  pattern = { 'NeogitBranchCheckout', 'NeogitPullComplete' },
+  callback = function()
+    vim.cmd 'set autoread | checktime'
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  desc = 'Fetch all on Negit open',
+  group = vim.api.nvim_create_augroup('neogit_fetch_on_open', { clear = true }),
+  pattern = 'NeogitStatusRefreshed',
+  callback = fetch,
+})
+
 return {
   'NeogitOrg/neogit',
   event = 'VeryLazy',
