d69f7254392b5f62dd9e40ca1eb8863189407a5d
sol-un <solovyev833@gmail.com>
2025-12-24 14:59:20 +0300

    set up TS

---
+++ lua/plugins/lang/ts.lua
@@ -0,0 +1,44 @@
+-- eslint and eslint_d will create duplicate diagnostics
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Hide diagnostics from eslint',
+  callback = function()
+    local ns = require('lint').get_namespace 'eslint'
+    vim.diagnostic.config({ virtual_lines = false, virtual_text = false, underline = false }, ns)
+  end,
+})
+
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function()
+      require('nvim-treesitter').install { 'ecma', 'javascript', 'typescript', 'tsx', 'jsdoc' }
+    end,
+  },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed or {}, { 'ts_ls', 'eslint', 'eslint_d' })
+    end,
+  },
+  {
+    'mfussenegger/nvim-lint', -- Linters
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = { linters_by_ft = { typescript = { 'eslint' }, javascript = { 'eslint' } } },
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        typescript = { 'eslint_d', 'prettier', stop_after_first = true },
+        javascript = { 'eslint_d', 'prettier', stop_after_first = true },
+      },
+    },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = function()
+      vim.lsp.enable 'ts_ls'
+      vim.lsp.enable 'eslint'
+    end,
+  },
+}
