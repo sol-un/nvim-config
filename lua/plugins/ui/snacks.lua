248a9d5f25f4f7905458eb0d5facfc7164ecabd9
sol-un <solovyev833@gmail.com>
2025-12-27 15:22:41 +0300

    add open in trouble from snacks keymap

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -1,42 +1,57 @@
 return {
   'folke/snacks.nvim',
   event = 'VeryLazy',
-  ---@module 'snacks'
-  ---@type snacks.Config
-  opts = {
-    notifier = {},
-    indent = {},
-    scroll = {},
-    statuscolumn = {
-      left = { 'git', 'sign' }, -- priority of signs on the left (high to low)
-      right = { 'fold' }, -- priority of signs on the right (high to low)
-      folds = { open = true },
-    },
-    bigfile = {},
-    picker = {
-      win = { wo = { wrap = true } },
-      sources = {
-        explorer = {
-          hidden = true,
-          ignored = true,
-          layout = {
-            fullscreen = false,
-            preview = 'main',
+  opts = function(_, opts)
+    ---@module 'snacks'
+    ---@type snacks.Config
+    local config = {
+      notifier = {},
+      indent = {},
+      scroll = {},
+      statuscolumn = {
+        left = { 'git', 'sign' }, -- priority of signs on the left (high to low)
+        right = { 'fold' }, -- priority of signs on the right (high to low)
+        folds = { open = true },
+      },
+      bigfile = {},
+      picker = {
+        actions = require('trouble.sources.snacks').actions,
+        win = {
+          wo = { wrap = true },
+          input = {
+            keys = {
+              ['<C-q>'] = {
+                'trouble_open',
+                mode = { 'n', 'i' },
+              },
+            },
           },
         },
-        select = { layout = { fullscreen = false } },
-        lines = { layout = { fullscreen = false } },
-        files = {
-          hidden = true,
-          ignored = true,
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+            layout = {
+              fullscreen = false,
+              preview = 'main',
+            },
+          },
+          select = { layout = { fullscreen = false } },
+          lines = { layout = { fullscreen = false } },
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          grep = { hidden = true },
+          grep_word = { hidden = true },
+          recent = { hidden = true, ignored = true },
         },
-        grep = { hidden = true },
-        grep_word = { hidden = true },
-        recent = { hidden = true, ignored = true },
+        layout = {
+          fullscreen = true,
+        },
       },
-      layout = {
-        fullscreen = true,
-      },
-    },
-  },
+    }
+
+    return vim.tbl_deep_extend('force', opts or {}, config)
+  end,
 }
