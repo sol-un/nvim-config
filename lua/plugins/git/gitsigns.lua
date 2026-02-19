0d116d08577afb4143f29d371f0d9fc9bdff426a
sol-un <solovyev833@gmail.com>
2026-02-19 10:00:20 +0300

    clean up gitsigns keymaps

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -23,12 +23,38 @@ return {
     local gitsigns = require 'gitsigns'
 
     require('which-key').add {
-      { '<Leader>gp', gitsigns.preview_hunk_inline, desc = 'Preview Git' },
-      { '<Leader>gr', gitsigns.reset_hunk, desc = 'Reset Git hunk' },
-      { '<Leader>gR', gitsigns.reset_buffer, desc = 'Reset Git buffer' },
-      { '<Leader>gs', gitsigns.stage_hunk, desc = 'Stage/Unstage Git hunk' },
-      { '<Leader>gS', gitsigns.stage_buffer, desc = 'Stage Git buffer' },
-      { '<Leader>gb', gitsigns.blame, desc = 'Git blame' },
+      { '<Leader>gp', gitsigns.preview_hunk_inline, desc = 'Preview diff' },
+      { '<Leader>gr', gitsigns.reset_hunk, desc = 'Reset hunk' },
+      {
+        '<Leader>gr',
+        function()
+          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end,
+        desc = 'Reset hunk',
+        mode = 'v',
+      },
+      { '<Leader>gR', gitsigns.reset_buffer, desc = 'Reset buffer' },
+      { '<Leader>gs', gitsigns.stage_hunk, desc = 'Stage/Unstage hunk' },
+      {
+        '<Leader>gs',
+        function()
+          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end,
+        desc = 'Stage hunk',
+        mode = 'v',
+      },
+      {
+        '<Leader>gS',
+        function()
+          if #gitsigns.get_hunks(0) > 0 then
+            gitsigns.stage_buffer()
+          else
+            gitsigns.reset_buffer_index()
+          end
+        end,
+        desc = 'Stage/Unstage buffer',
+      },
+      { '<Leader>gb', gitsigns.blame, desc = 'Blame file' },
       {
         ']h',
         function()
@@ -42,22 +68,6 @@ return {
           gitsigns.nav_hunk 'prev'
         end,
         desc = 'Previous Git hunk',
-      },
-      {
-        '<Leader>gs',
-        function()
-          gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end,
-        desc = 'Stage Git hunk',
-        mode = 'v',
-      },
-      {
-        '<Leader>gr',
-        function()
-          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end,
-        desc = 'Reset Git hunk',
-        mode = 'v',
       },
       { 'ih', ':<C-U>Gitsigns select_hunk<CR>', desc = 'inside Git hunk', mode = { 'x', 'o' } },
     }
