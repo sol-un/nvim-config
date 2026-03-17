8117786636c05717e618306d10bcba8bd3160b86
sol-un <solovyev833@gmail.com>
2026-03-17 10:03:42 +0300

    update css_variables ls lookupFiles

--- after/lsp/css_variables.lua
+++ after/lsp/css_variables.lua
@@ -1,5 +1,8 @@
 return {
   before_init = function(_, config)
-    vim.list_extend(config.settings.cssVariables.lookupFiles, { 'node_modules/@skbkontur/colors/colors.css' })
+    vim.list_extend(config.settings.cssVariables.lookupFiles, {
+      'node_modules/@skbkontur/colors/colors.css',
+      'node_modules/@skbkontur/colors/tokens/brand-blue-deep_accent-brand.css',
+    })
   end,
 }
