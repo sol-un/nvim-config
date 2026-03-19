0087bc25ac0d52aea74f1fce7cb549f365d63432
sol-un <solovyev833@gmail.com>
2026-03-19 19:21:19 +0300

    add dispose all overseer tasks keymap

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,6 +1,8 @@
 return {
   'stevearc/overseer.nvim',
   cmd = { 'OverseerRun', 'OverseerToggle' },
+  ---@module 'overseer'
+  ---@type overseer.SetupOpts
   opts = {
     dap = false,
     task_list = {
@@ -16,6 +18,18 @@ return {
       { '<Leader>ot', '<cmd>OverseerToggle! right<cr>', desc = 'Toggle task list' },
       { '<Leader>or', '<cmd>OverseerRun<cr>', desc = 'Run task' },
       { '<Leader>oa', '<cmd>OverseerTaskAction<cr>', desc = 'Task action' },
+      {
+        '<Leader>od',
+        function()
+          local overseer = require 'overseer'
+          local tasks = overseer.list_tasks()
+
+          for _, task in ipairs(tasks) do
+            overseer.run_action(task, 'dispose')
+          end
+        end,
+        desc = 'Dispose all tasks',
+      },
     }
   end,
   keys = '<Leader>o',
