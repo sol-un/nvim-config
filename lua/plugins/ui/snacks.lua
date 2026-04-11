bc8a728bcd1872b493c6d106679dfd32d3756dfe
sol-un <solovyev833@gmail.com>
2026-04-11 19:21:07 +0300

    enable snacks input

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -75,6 +75,7 @@ return {
     ---@type snacks.Config
     local config = {
       notifier = {},
+      input = { win = { row = vim.o.lines } },
       indent = {},
       scroll = {},
       words = {},
