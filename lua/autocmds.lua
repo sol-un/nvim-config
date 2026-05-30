04c059dcc52e3e5995cd0dd5b9b51cbcb58eb803
sol-un <solovyev833@gmail.com>
2026-05-30 08:33:15 +0300

    clean up

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -18,15 +18,12 @@ vim.api.nvim_create_autocmd('FileType', {
   desc = 'Start treesitter',
   callback = function(args)
     local buf, filetype = args.buf, args.match
-
     local language = vim.treesitter.language.get_lang(filetype)
-    if not language then
+
+    if not language or not vim.treesitter.language.add(language) then
       return
     end
 
-    if not vim.treesitter.language.add(language) then
-      return
-    end
     vim.treesitter.start(buf, language)
   end,
 })
