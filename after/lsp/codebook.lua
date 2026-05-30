58be03fa97b16e3aa01364778ef327de34b29720
sol-un <solovyev833@gmail.com>
2026-05-30 13:05:08 +0300

    disable virtual text diagnostics for codebook

---
+++ after/lsp/codebook.lua
@@ -0,0 +1,8 @@
+---@type vim.lsp.Config
+return {
+  -- Disable virtual text diagnostics specifically for codebook to reduce visual noise
+  on_attach = function(client)
+    local ns = vim.lsp.diagnostic.get_namespace(client.id)
+    vim.diagnostic.config({ virtual_text = false }, ns)
+  end,
+}
