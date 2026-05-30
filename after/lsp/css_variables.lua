d04fdadbdb588080b8ee668156052cfcbdd45b77
sol-un <solovyev833@gmail.com>
2026-05-30 13:05:00 +0300

    type LSP configs

--- after/lsp/css_variables.lua
+++ after/lsp/css_variables.lua
@@ -1,3 +1,4 @@
+---@type vim.lsp.Config
 return {
   before_init = function(_, config)
     vim.list_extend(config.settings.cssVariables.lookupFiles, {
--- after/lsp/jsonls.lua
+++ after/lsp/jsonls.lua
@@ -1,6 +1,7 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+---@type vim.lsp.Config
 return {
   capabilities = capabilities,
   before_init = function(_, config)
