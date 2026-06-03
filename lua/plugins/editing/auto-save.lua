df8d67a36ce61fdefb1627c3bea1d265a8b98b54
sol-un <solovyev833@gmail.com>
2026-06-03 12:20:32 +0300

    auto-save on FocusLost

--- lua/plugins/editing/auto-save.lua
+++ lua/plugins/editing/auto-save.lua
@@ -3,10 +3,7 @@ return {
   opts = {
     trigger_events = {
       immediate_save = {},
-      -- TODO:
-      -- Should also enable 'FocusLost', but it messes with markdown files when swiching language.
-      -- Adjus options dynamically in markdown lang settings
-      defer_save = { 'BufLeave', 'QuitPre', 'VimSuspend' },
+      defer_save = { 'BufLeave', 'QuitPre', 'VimSuspend', 'FocusLost' },
     },
     debounce_delay = 100,
   },
