a5f59f20b607650d22748c85c951b950a69a0bc3
sol-un <solovyev833@gmail.com>
2026-06-26 09:28:48 +0300

    set up image

--- lua/plugins/ui/smear-cursor.lua
+++ lua/plugins/ui/smear-cursor.lua
@@ -1,6 +1,6 @@
 return {
   'sphamba/smear-cursor.nvim',
   -- NOTE: kitty features a built-in cursor smear effect; add "cursor_trail 1" to kitty.conf to enable
-  cond = os.getenv 'TERM' ~= 'xterm-kitty',
+  cond = require('utils').is_kitty(),
   opts = { min_horizontal_distance_smear = 2, min_vertical_distance_smear = 2 },
 }
--- lua/utils.lua
+++ lua/utils.lua
@@ -9,6 +9,10 @@ local M = {
   is_wsl = function()
     return vim.fn.isdirectory '/mnt/c' == 1
   end,
+  --- @return boolean
+  is_kitty = function()
+    return os.getenv 'TERM' ~= 'xterm-kitty'
+  end,
   --- @return string
   get_cwd_name = function()
     return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
