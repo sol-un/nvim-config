b3d4dc6ef864bdb090eac54d4b750c5a76863239
sol-un <solovyev833@gmail.com>
2026-06-26 17:26:35 +0300

    adjust blink-cmp settings

--- lua/plugins/editing/blink-cmp.lua
+++ lua/plugins/editing/blink-cmp.lua
@@ -1,103 +1,96 @@
 return {
-  {
-    'saghen/blink.cmp',
-    build = function()
-      require('blink.cmp').build():wait(60000)
-    end,
-    event = { 'InsertEnter', 'CmdlineEnter' },
-    dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = 'make install_jsregexp',
-        dependencies = {
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
-        opts = {},
+  'saghen/blink.cmp',
+  build = function()
+    require('blink.cmp').build():wait(60000)
+  end,
+  event = { 'InsertEnter', 'CmdlineEnter' },
+  dependencies = {
+    {
+      'L3MON4D3/LuaSnip',
+      version = '2.*',
+      build = 'make install_jsregexp',
+      dependencies = {
+        'rafamadriz/friendly-snippets',
+        config = function()
+          require('luasnip.loaders.from_vscode').lazy_load()
+        end,
       },
-      { 'moyiz/blink-emoji.nvim', cond = false },
-      'folke/lazydev.nvim',
-      'saghen/blink.lib',
+      opts = {},
     },
-    --- @module 'blink.cmp'
-    --- @type blink.cmp.Config
-    --- @diagnostic disable: missing-fields
-    opts = {
-      cmdline = {
-        enabled = true,
-        keymap = {
-          preset = 'cmdline',
-        },
-        completion = {
-          list = { selection = { preselect = false } },
-          menu = {
-            auto_show = function()
-              return vim.fn.getcmdtype() == ':'
-            end,
-          },
-          ghost_text = { enabled = true },
-        },
-      },
+    'saghen/blink.lib',
+    'sol-un/blink-emoji.nvim',
+    'folke/lazydev.nvim',
+  },
+  --- @module 'blink.cmp'
+  --- @type blink.cmp.Config
+  --- @diagnostic disable: missing-fields
+  opts = {
+    cmdline = {
+      enabled = true,
       keymap = {
-        preset = 'default',
-      },
-      appearance = {
-        nerd_font_variant = 'mono',
+        preset = 'cmdline',
       },
       completion = {
-        documentation = { auto_show = true, window = { border = 'rounded' } },
-        menu = { border = 'rounded' },
-      },
-      sources = {
-        default = {
-          'lsp',
-          'path',
-          'snippets',
-          'lazydev',
-          'buffer',
-          -- 'emoji',
+        list = { selection = { preselect = false } },
+        menu = {
+          auto_show = function()
+            return vim.fn.getcmdtype() == ':'
+          end,
         },
-        providers = {
-          lsp = {
-            score_offset = 100,
-            fallbacks = {},
+        ghost_text = { enabled = true },
+      },
+    },
+    keymap = {
+      preset = 'default',
+    },
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+    completion = {
+      documentation = { auto_show = true, window = { border = 'rounded' } },
+      menu = { border = 'rounded' },
+    },
+    sources = {
+      default = {
+        'lsp',
+        'path',
+        'snippets',
+        'lazydev',
+        'buffer',
+        'emoji',
+      },
+      providers = {
+        lsp = {
+          score_offset = 100,
+          fallbacks = {},
+        },
+        buffer = {
+          opts = {
+            -- filter to only "normal" buffers
+            get_bufnrs = function()
+              return vim.tbl_filter(function(bufnr)
+                local buftype = vim.bo[bufnr].buftype
+                return buftype == ''
+              end, vim.api.nvim_list_bufs())
+            end,
           },
-          snippets = {
-            score_offset = 100,
-          },
-          buffer = {
-            opts = {
-              -- filter to only "normal" and help buffers
-              get_bufnrs = function()
-                return vim.tbl_filter(function(bufnr)
-                  local buftype = vim.bo[bufnr].buftype
-                  return buftype == '' or buftype == 'help'
-                end, vim.api.nvim_list_bufs())
-              end,
-            },
-          },
-          lazydev = {
-            module = 'lazydev.integrations.blink',
-          },
-          -- emoji = {
-          --   name = 'emoji',
-          --   module = 'blink-emoji',
-          --   score_offset = function(ctx)
-          --     return ctx.trigger.initial_character == ':' and 100 or 0
-          --   end,
-          -- },
+        },
+        lazydev = {
+          module = 'lazydev.integrations.blink',
+        },
+        emoji = {
+          name = 'emoji',
+          module = 'blink-emoji',
+          score_offset = function(ctx)
+            return ctx.trigger.initial_character == ':' and 100 or 0
+          end,
         },
       },
-      snippets = { preset = 'luasnip' },
-      fuzzy = {
-        sorts = { 'exact', 'score', 'sort_text' },
-        implementation = 'prefer_rust_with_warning',
-      },
+    },
+    snippets = { preset = 'luasnip' },
+    fuzzy = {
+      sorts = { 'exact', 'score', 'sort_text' },
+      implementation = 'prefer_rust_with_warning',
     },
   },
 }
