a9daaf341ca386b8720ad28724ff2b470fda46a4
sol-un <solovyev833@gmail.com>
2026-07-02 09:30:35 +0300

    don't auto-save gitcommit filetype

--- lua/plugins/editing/auto-save.lua
+++ lua/plugins/editing/auto-save.lua
@@ -1,3 +1,7 @@
+local excluded_filetypes = {
+  'gitcommit',
+}
+
 return {
   'okuuva/auto-save.nvim',
   opts = {
@@ -5,6 +9,10 @@ return {
       immediate_save = {},
       defer_save = { 'BufLeave', 'QuitPre', 'VimSuspend', 'FocusLost' },
     },
+    condition = function(buf)
+      local ft = vim.fn.getbufvar(buf, '&filetype')
+      return not vim.tbl_contains(excluded_filetypes, ft)
+    end,
     debounce_delay = 100,
   },
 }
