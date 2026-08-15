af53f483a6de64d7229ef773d2b3b648a82ea6d3
sol-un <solovyev833@gmail.com>
2026-08-15 11:02:58 +0300

    drop nvim-web-devicons

--- lua/plugins/ui/icons.lua
+++ lua/plugins/ui/icons.lua
@@ -1,4 +1,12 @@
 return {
-  { 'nvim-mini/mini.icons' },
-  { 'nvim-tree/nvim-web-devicons' },
+  {
+    'nvim-mini/mini.icons',
+    opts = {},
+    init = function()
+      package.preload['nvim-web-devicons'] = function()
+        require('mini.icons').mock_nvim_web_devicons()
+        return package.loaded['nvim-web-devicons']
+      end
+    end,
+  },
 }
