73e84d1d7f711c5024406701aa4c50bc25aaf245
sol-un <solovyev833@gmail.com>
2026-08-15 08:45:29 +0300

    debloat markdown config

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -2,24 +2,6 @@ local markdownlint = require('null-ls').builtins.diagnostics.markdownlint.with {
   extra_args = { '--disable', 'MD013', 'MD033' },
 }
 
-local keymaps_meta = {
-  { desc = 'Bold', key = 'b', symbol = '**' },
-  { desc = 'Italic', key = 'i', symbol = '_' },
-  { desc = 'Code', key = 'c', symbol = '`' },
-  { desc = 'Strikethrough', key = 's', symbol = '~~' },
-}
-
-local add_md_link = function(rhs)
-  local link = vim.fn.input 'URL'
-  if link == nil or link == '' then
-    return
-  end
-
-  local rhs_with_link = string.format(rhs, link)
-  local input = vim.api.nvim_replace_termcodes(rhs_with_link, true, false, true)
-  vim.api.nvim_feedkeys(input, 'n', false)
-end
-
 return {
   {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
@@ -49,46 +31,7 @@ return {
     --- @type render.md.UserConfig
     opts = {
       preset = 'obsidian',
-      anti_conceal = { enabled = false },
       file_types = { 'markdown', 'gitlab' },
     },
-    init = function()
-      require('which-key').add {
-        '<Leader>m',
-        group = 'Markdown',
-        icon = { icon = ' ', color = 'grey' },
-      }
-      local set = require('snacks').keymap.set
-
-      set('n', '<Leader>mt', '<cmd>RenderMarkdown buf_toggle<cr>', { ft = 'markdown', desc = 'Toggle preview' })
-
-      vim.iter(keymaps_meta):each(function(keymap_meta)
-        local key = keymap_meta.key
-        local sym = keymap_meta.symbol
-        local desc = keymap_meta.desc
-
-        set('v', '<Leader>m' .. key, string.format('c%s<c-r>"%s<esc>', sym, sym), { ft = 'markdown', desc = desc })
-        set('n', '<Leader>mw' .. key, string.format('viwc%s<c-r>"%s<esc>', sym, sym), { ft = 'markdown', desc = desc })
-        set('n', '<Leader>mW' .. key, string.format('viWc%s<c-r>"%s<esc>', sym, sym), { ft = 'markdown', desc = desc })
-      end)
-
-      set('v', '<Leader>ml', function()
-        add_md_link 'c[<c-r>"](%s)<esc>'
-      end, { ft = 'markdown', desc = 'Link' })
-
-      set('n', '<Leader>ml', function()
-        add_md_link 'viwc[<c-r>"](%s)<esc>'
-      end, { ft = 'markdown', desc = 'Link' })
-
-      set('n', '<Leader>mC', function()
-        local language = vim.fn.input 'Language'
-        if language == nil or language == '' then
-          return
-        end
-
-        local block_top = '```' .. language
-        vim.api.nvim_put({ block_top, '```' }, 'c', true, true)
-      end, { ft = 'markdown', desc = 'Insert code block' })
-    end,
   },
 }
