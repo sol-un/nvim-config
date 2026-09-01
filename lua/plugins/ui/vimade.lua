71e1e9df2fb3da156b7ff8bc40dcef063bd58ed6
Соловьев Андрей Андреевич <solovev.aa@kontur>
2026-09-01 08:43:27 +0300

    reenable vimade on windows

---
+++ lua/plugins/ui/vimade.lua
@@ -0,0 +1,28 @@
+vim.api.nvim_create_autocmd('FocusLost', {
+  desc = 'Fade on blur',
+  group = vim.api.nvim_create_augroup('vimade_focus_fading', { clear = false }),
+  callback = function()
+    pcall(vim.cmd, 'VimadeEnable')
+  end,
+})
+
+vim.api.nvim_create_autocmd('FocusGained', {
+  desc = 'Unfade on start/focus',
+  group = vim.api.nvim_create_augroup('vimade_focus_fading', { clear = false }),
+  callback = function()
+    pcall(vim.cmd, 'VimadeDisable')
+  end,
+})
+
+-- Only use vimade to fade the entire editor when the window is blurred
+return {
+  {
+    'TaDaa/vimade',
+    event = 'FocusLost',
+    enabled = require('utils').is_windows(),
+    opts = {
+      recipe = { 'default', { animate = true } },
+      enablefocusfading = true,
+    },
+  },
+}
