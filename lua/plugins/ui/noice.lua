cfc2b2830adab7aa81c8d9f1ee4d1ca112c9f1e2
sol-un <solovyev833@gmail.com>
2026-09-08 18:11:34 +0300

    disable "No information available" on lsp hover

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -9,6 +9,7 @@ return {
   --- @diagnostic disable: missing-fields
   opts = {
     lsp = {
+      hover = { silent = true },
       override = {
         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
         ['vim.lsp.util.stylize_markdown'] = true,
