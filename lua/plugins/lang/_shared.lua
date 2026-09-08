a59b1cf211ce1210eb918e9461c252bdf698ca86
sol-un <solovyev833@gmail.com>
2026-09-08 10:27:10 +0300

    fix mason-tool-installer not auto-updating

--- lua/plugins/lang/_shared.lua
+++ lua/plugins/lang/_shared.lua
@@ -19,7 +19,6 @@ return {
   },
   {
     'WhoIsSethDaniel/mason-tool-installer.nvim', -- automatically installs mason packages
-    event = 'VeryLazy',
     opts_extend = { 'ensure_installed' },
     --- @module 'mason-tool-installer'
     --- @type MasonToolInstallerSettings
