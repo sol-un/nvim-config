8851925bb6068ce5ee2d9fc8e00a30eb39214ea1
sol-un <solovyev833@gmail.com>
2026-09-24 12:47:14 +0300

    support cyrillic chars in flash

--- lua/plugins/editing/flash.lua
+++ lua/plugins/editing/flash.lua
@@ -1,3 +1,17 @@
+local cyrillic_alphabet = 'ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
+local cyrillic_labels = 'олджавыфгнрткепимйцуячсшщзьбюАВЫФОЛДЖЙЦУКЕНГШЩЗ'
+
+local is_current_char_cyrillic = function()
+  local current_char = unpack(vim.fn.getregion(vim.fn.getpos '.', vim.fn.getpos '.'))
+
+  if current_char == '' then
+    return false
+  end
+
+  local index = string.find(cyrillic_alphabet, current_char, nil, true)
+  return index ~= nil
+end
+
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
@@ -9,7 +23,20 @@ return {
     {
       's',
       function()
-        require('flash').jump()
+        local flash = require 'flash'
+        local kde_layout = require 'kde-layout'
+
+        if not is_current_char_cyrillic() then
+          flash.jump()
+        else
+          local previous_layout = kde_layout.get_layout()
+
+          kde_layout.set_layout 'ru'
+          flash.jump { labels = cyrillic_labels }
+          if previous_layout then
+            kde_layout.set_layout(previous_layout)
+          end
+        end
       end,
       desc = 'Flash',
       mode = { 'n', 'x', 'o' },
